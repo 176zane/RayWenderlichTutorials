7670d98f137bd47595b44905fb954eacb83cf8bd
姚七六 <176zane@gmail.com>
2017-06-07 20:46:02 +0800

    first commit

---
+++ Auto Layout Tutorial  Part 1/StrutsProblem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StrutsProblem
+//
+//  Created by Deep Thought on 2017/5/9.
+//  Copyright © 2017年 Zane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var topLeftView: UIView!
+    @IBOutlet weak var topRightView: UIView!
+    @IBOutlet weak var bottomView: UIView!
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+    }
+        
+
+
+}
+
